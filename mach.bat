abc0acc26da609b68371979e6762c8497e85c8de
Xyraniz <xyraniz@protonmail.com>
2026-09-21 07:57:16 -0600

    Refactor mach.bat to prioritize vpython3

--- mach.bat
+++ mach.bat
@@ -1,9 +1,26 @@
 @echo off
 setlocal
-where py >nul 2>nul
-if %errorlevel% equ 0 (
-  py -3 "%~dp0mach" %*
-) else (
-  python "%~dp0mach" %*
-)
+
+where.exe vpython3 >nul 2>nul
+if not errorlevel 1 goto run_vpython3
+
+where.exe py >nul 2>nul
+if not errorlevel 1 goto run_py
+
+where.exe python >nul 2>nul
+if not errorlevel 1 goto run_python
+
+>&2 echo Could not find vpython3, py, or python on PATH.
+exit /b 9009
+
+:run_vpython3
+vpython3 "%~dp0mach" %*
+exit /b %errorlevel%
+
+:run_py
+py -3 "%~dp0mach" %*
+exit /b %errorlevel%
+
+:run_python
+python "%~dp0mach" %*
 exit /b %errorlevel%
